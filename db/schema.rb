5920f76e51da0a3080c4f2dba3f44ddfae83864d
Tessa Podbury <65106860+tepo-design@users.noreply.github.com>
2021-05-22 12:17:01 +1000

    fixing credentials

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_18_054756) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "image_name"
     t.float "cost"

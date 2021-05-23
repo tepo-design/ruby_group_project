204fa8d6f0ce42542a9d0db1f0d84cd7ffe1604e
Tessa Podbury <65106860+tepo-design@users.noreply.github.com>
2021-05-23 12:53:29 +1000

    fixing mail form

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,13 +1,10 @@
 class Email < MailForm::Base
-    # attribute :name, :validate => true
     attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-    # attribute :message, :validate => true
 
     def headers 
         {
-        :subject => "Email Confirmation",
-        :to => "#{email}",
-        # :from => %("#{name}" <#{email}>)
+        :subject => "Newsletter Confirmation",
+        # :to => "#{email}",
         }
     end
 end

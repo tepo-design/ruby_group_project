fb3a3d96d2482c2cc308e2782625611ab75e8087
Tessa Podbury <65106860+tepo-design@users.noreply.github.com>
2021-05-23 10:46:11 +1000

    updated email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     authentication: :plain,
-    user_name:      'apikey',
+    user_name:      ENV['tessa.podbury@gmail.com'],
     password:       ENV['SENDGRID_API_KEY']
   }
 end
